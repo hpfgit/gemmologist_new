36bb93d624e3f27bda85453b18b1988286c9c50b
haopengfei <1185101239@qq.com>
2019-12-19 19:54:42 +0800

    鉴定师

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ const config = {
   development: {
     host: "https://stg.tosneaker.com",
     imgUrl: "http://static-stg.tosneaker.com",
-    domain: "http://static-stg.tosneaker.com",
+    domain: "http://static.tosneaker.com",
     qiniuUrl: "http://static-stg.tosneaker.com/image/appraisal/"
   },
   production: {
